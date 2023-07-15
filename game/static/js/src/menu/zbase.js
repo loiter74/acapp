3819ae864c112ca546b162b4d335c45d67025910
acs <302626787@qq.com>
2023-07-15 20:49:27 +0800

    get menu background

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -8,12 +8,3 @@ class AcGameMenu{
         this.root.$ac_game.append(this.$menu);
     }
 }
-class AcGame{
-    constructor(id){
-     this.id = id;
-     this.$ac_game = $('#' + id);
-     this.menu = new AcGameMenu(this);
-     console.log("acgame 启动！");
-    }
-}
-
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,9 @@
 class AcGame{
     constructor(id){
+     this.id = id;
+     this.$ac_game = $('#' + id);
+     this.menu = new AcGameMenu(this);
+     console.log("acgame 启动！");
     }
 }
 
