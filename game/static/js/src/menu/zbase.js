f467f16534210da781e5a6e5f89d55fa701a7ac8
acs <302626787@qq.com>
2023-07-15 21:12:49 +0800

    menu 1:30

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -3,8 +3,20 @@ class AcGameMenu{
         this.root = root;
         this.$menu = $(`
 <div class="ac-game-menu">
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+            单人模式
+        </div>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi">
+            多人模式
+        </div>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+            设置
+        </div>
+    </div>
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
+        this.$single = this.$menu.find('.')
     }
 }
