201c7de76040b5a6a323fa29d8564ea676322960
acs <302626787@qq.com>
2023-07-18 20:03:52 +0800

    playgournd click

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -57,13 +57,147 @@ class AcGameMenu{
 
 
 }
+let AC_GAME_OBJECTS = [];
+
+
+class AcGameObject{
+    constructor(){
+        AC_GAME_OBJECTS.push(this);
+
+        this.has_called_start = false; //是否执行过start函数
+        this.timedelta = 0; //当前帧距离上一帧的时间间隔
+    }
+
+    start(){ //只会在第一帧执行
+    }
+
+    update(){ //每帧执行
+
+    }
+
+    on_destroy(){ //被销毁前执行
+
+    }
+
+    destroy(){ //删除物体
+        this.on_destroy();
+        for (let i = 0; i < AC_GAME_OBJECTS.length; i++){
+            if (AC_GAME_OBJECTS[i] === this){
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp){
+    for (let i = 0; i < AC_GAME_OBJECTS.length; i++) {
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start){
+            obj.start();
+            obj.has_called_start = true;
+        }else{
+            obj.timedelta= timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);
+class GameMap extends AcGameObject{
+    constructor(playground){
+        super();
+        this.playground = playground;
+        this.$canvas = $("<canvas></canvas>");
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.height = this.playground.height;
+        this.ctx.canvas.width = this.playground.width;
+        this.playground.$playground.append(this.$canvas);
+    }
+
+    start(){
+    }
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.fillStyle = "rgba(0,0,0, 0.2)";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+}
+class Player extends AcGameObject{
+    constructor(playground, x, y, radius, color, speed, is_me){
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x
+        this.y = y
+        this.vx = 0;
+        this.vy = 0;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.eps = 0.1
+    }
+
+    start(){
+        if (this.is_me){
+            this.add_listening_events();
+        }
+    }
+
+    add_listening_events(){
+        let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu", function(){
+            return false;
+        });
+        this.playground.game_map.$canvas.mousedown(function(e){
+            if (e.which ===3){
+                outer.move_to(e.clientX, e.clientY);
+            }
+        });
+    }
+
+    move_to(tx, ty){
+        console.log("move to", tx, ty);
+    }
+
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+
+}
 class AcGamePlayground{
     constructor(root){
         this.root = root;
-        this.$playground = $("<div>游戏中</div>");
+        this.$playground = $('<div class="ac-game-playground"></div>');
 
-        this.hide();
+//      this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.height = this.$playground.height();
+        this.width = this.$playground.width();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width/2, this.height/2, this.height*0.05, "white", this.height*0.15, true))
+
+
+        this.start();
     }
 
     start(){
@@ -82,7 +216,7 @@ export class AcGame{
     constructor(id){
      this.id = id;
      this.$ac_game = $('#' + id);
-     this.menu = new AcGameMenu(this);
+//     this.menu = new AcGameMenu(this);
      this.playground = new AcGamePlayground(this);
      console.log("acgame 启动！");
 
